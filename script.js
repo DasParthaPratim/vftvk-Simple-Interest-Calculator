5c8aef21fee87f41fc6481bc24c04059680dfa55
Partha <partha.das31@gmail.com>
2021-02-15 18:32:25 +0000

    uploading final draft

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ function compute(){
      var Rate = document.getElementById("rate").value;
      var Years= document.getElementById("years").value;
 
+     //code block to get final amount with interest 
+
     Interest= (Principal*Years*Rate/100);
+    var final_amount=(parseInt(Principal)+parseInt(Interest));
 
      //code to covert years to actual year
     var date=new Date();
     var present_year=date.getFullYear();
     var final_year=(parseInt(present_year)+ parseInt(Years));
 
+    //code block to show message on submit
      var output="If you deposit "+ Principal +
             " at an interest of "+ Rate +"%"+
-            " you will receive an amount of "+ Interest +
+            " you will receive an amount of "+ final_amount +
             " in the year "+ final_year
         document.getElementById("result").innerHTML=output
         console.log(output)
@@ -22,10 +26,10 @@ function compute(){
 
     //function to check principal amount
     function validate(){
-        var amount= document.getElementById("principal");
-        if ((amount < 0) || (amount = 0)){
-            alert("Enter a Postive number");
-            document.getElementById("principal").focus;
+        var amount= parseInt(document.getElementById("principal").value);
+        if ((amount < 0) || (amount == 0)){
+            alert("Enter a Postive number");//shows aler message
+            document.getElementById("principal").focus;//after clicking okay brings the focus to element
         }
     }
 
