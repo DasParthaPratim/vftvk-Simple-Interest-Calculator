bd2f70b1ee6864922ed1b422e8c0bf7fa6e38aa0
Partha <partha.das31@gmail.com>
2021-02-15 22:35:25 +0000

    uploading final code for testing

--- script.js
+++ script.js
@@ -4,35 +4,37 @@ function compute(){
      var Principal = document.getElementById("principal").value;
      var Rate = document.getElementById("rate").value;
      var Years= document.getElementById("years").value;
+    
+     //calling function to validate amount
+    validate();
+    //code block to get final amount with interest 
 
-     //code block to get final amount with interest 
-
-    Interest= (Principal*Years*Rate/100);
-    var final_amount=(parseInt(Principal)+parseInt(Interest));
+    interest= (Principal*Years*Rate/100);
+    var final_amount=(parseInt(Principal)+parseInt(interest));
 
      //code to covert years to actual year
     var date=new Date();
     var present_year=date.getFullYear();
     var final_year=(parseInt(present_year)+ parseInt(Years));
 
+
     //code block to show message on submit
-     var output="If you deposit "+ Principal +
-            " at an interest of "+ Rate +"%"+
-            " you will receive an amount of "+ final_amount +
+     var output="If you deposit "+ Principal +","+ "</br>"
+            " at an interest of "+ Rate +"%."+"</br>"
+            " You will receive an amount of "+ final_amount + "</br>"
             " in the year "+ final_year
         document.getElementById("result").innerHTML=output
         console.log(output)
 }
-
     //function to check principal amount
     function validate(){
-        var amount= parseInt(document.getElementById("principal").value);
+    var amount= parseInt(document.getElementById("principal").value);
         if ((amount < 0) || (amount == 0)){
             alert("Enter a Postive number");//shows aler message
-            document.getElementById("principal").focus;//after clicking okay brings the focus to element
+            document.getElementById("principal").focus();//after clicking OK brings the focus to element
+            javascript_abort();
         }
     }
-
     //function to display slider value
 
     function showSlider() {
@@ -40,3 +42,7 @@ function compute(){
     document.getElementById('slider').innerHTML = input //displays value to span
       console.log(input)
      }
+     //function to abort JS execution in case of incorrect amount
+     function javascript_abort(){
+        throw new Error('Pupose of this fuction is to abort execution,not an error');
+        }
