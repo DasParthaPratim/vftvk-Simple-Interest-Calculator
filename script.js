b7dd9d31763e552e6c83e40e227205c11eaf400e
Partha <partha.das31@gmail.com>
2021-02-15 16:25:52 +0000

    uploading latest code

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
+function compute(){
 
-function compute()
-{
-     Principal = document.getElementById("principal").value;
-     Rate = document.getElementById("rate").value;
-     Years= document.getElementById("years").value;
 
-     Interest= (principal*years*rate/100);
+     var Principal = document.getElementById("principal").value;
+     var Rate = document.getElementById("rate").value;
+     var Years= document.getElementById("years").value;
 
-    //code to covert years to actual year
+    Interest= (Principal*Years*Rate/100);
+
+     //code to covert years to actual year
     var date=new Date();
     var present_year=date.getFullYear();
-    var final_year=new Date(present_year+years);
+    var final_year=(parseInt(present_year)+ parseInt(Years));
 
-        //code to show the text
-        document.getElementById("submit").onclick=function(){
-            showOutput()
-        }
-
-        function showOutput(){
-            var output="If you deposit "+ Principal+
-            "at an interest of "+Rate+"%"+
-            "you will receive an amount of "+Interest+
-            "in the year"+final_year
+     var output="If you deposit "+ Principal +
+            " at an interest of "+ Rate +"%"+
+            " you will receive an amount of "+ Interest +
+            " in the year "+ final_year
         document.getElementById("result").innerHTML=output
         console.log(output)
-        }
-        
 }
-   //function to check principal amount
+
+    //function to check principal amount
     function validate(){
-        if ((Principal < 0) || (Principal = 0)){
+        var amount= document.getElementById("principal");
+        if ((amount < 0) || (amount = 0)){
             alert("Enter a Postive number");
             document.getElementById("principal").focus;
-        }else{
-            break;
         }
     }
-        
+
     //function to display slider value
 
     function showSlider() {
     var input= document.getElementById("rate").value //gets the vaue onchange
     document.getElementById('slider').innerHTML = input //displays value to span
       console.log(input)
-        }
+     }
