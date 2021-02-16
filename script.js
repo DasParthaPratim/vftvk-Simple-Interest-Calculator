2b371c7dd98e92f798df482c1a8cb3265129e5df
DasParthaPratim <60557233+DasParthaPratim@users.noreply.github.com>
2021-02-16 01:17:00 +0100

    Update script.js
    final code upload

--- script.js
+++ script.js
@@ -7,30 +7,31 @@ function compute(){
     
      //calling function to validate amount
     validate();
-    //code block to get final amount with interest 
 
-    interest= (Principal*Years*Rate/100);
-    var final_amount=(parseInt(Principal)+parseInt(interest));
+     //code block to get final amount with interest 
+
+    Interest= (Principal*Years*Rate/100);
+    var final_amount=(parseInt(Principal)+parseInt(Interest));
 
      //code to covert years to actual year
     var date=new Date();
     var present_year=date.getFullYear();
     var final_year=(parseInt(present_year)+ parseInt(Years));
 
-
     //code block to show message on submit
-     var output="If you deposit "+ Principal +","
-            " at an interest of "+ Rate +"%."+"<br>"
-            " You will receive an amount of "+final_amount+
-            " in the year "+ final_year
+     var output="If you deposit "+ Principal+
+            ",<br>at an interest of "+ Rate +"%."+
+            "<br>You will receive an amount of "+ final_amount +
+            ",<br>in the year "+ final_year
         document.getElementById("result").innerHTML=output
         console.log(output)
 }
+
     //function to check principal amount
     function validate(){
     var amount= parseInt(document.getElementById("principal").value);
-        if ((amount < 0) || (amount == 0)){
-            alert("Enter a Postive number");//shows aler message
+        if ((amount < 0) || (amount == 0)||(document.getElementById("principal").value == "")){
+            alert("Enter a Postive Amount");//shows aler message
             document.getElementById("principal").focus();//after clicking OK brings the focus to element
             javascript_abort();
         }
@@ -45,4 +46,4 @@ function compute(){
      //function to abort JS execution in case of incorrect amount
      function javascript_abort(){
         throw new Error('Pupose of this fuction is to abort execution,not an error');
-        }
+        }	
